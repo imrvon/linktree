80a003b09c726aee82b4a62377679e1bc3221eac
VoN <hi@imrvon.com>
2022-11-03 18:20:04 +0100

    Remove Resizing from textarea

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,26 +4,28 @@ import Footer from '../components/Footer'
 function Contact() {
   return (
     <div className="contact">
-      <h1>Contact Me</h1>
-      <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      <header>
+        <h1 className='contact-h1'>Contact Me</h1>
+        <p className='contact-p'>Hi there, contact me to ask me about anything you have in mind.</p>
+      </header>
 
       <form action="">
-        <div  className='flex-div'>
+        <div className='flex-div'>
           <div>
-            <label htmlFor="">First name</label>
-            <input type="text" name="" id="first_name" placeholder='Enter your first name'/>
+            <label htmlFor="first_name">First name</label>
+            <input type="text" name="" id="first_name" placeholder='Enter your first name' required/>
           </div>
           <div>
-            <label htmlFor="">Last name</label>
-            <input type="text" name="" id="last_name" placeholder='Enter your last name'/>
+            <label htmlFor="last_name">Last name</label>
+            <input type="text" name="" id="last_name" placeholder='Enter your last name' required/>
           </div>
         </div>
-        <label htmlFor="">Email</label>
-        <input type="email" name="" id="email" placeholder='yourname@email.com'/>
-        <label htmlFor="">Message</label>
-        <textarea name="" id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." />
-        <input type="checkbox" name="" id="" /> <span>You agree to providing your data to Zuri who may contact you.</span>
-        <input type="submit" value="send message" />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="" id="email" placeholder='yourname@email.com' required/>
+        <label htmlFor="message">Message</label>
+        <textarea name="" id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." required/>
+        <div className='accept'><input type="checkbox" name="" id="" required /> <span>You agree to providing your data to Zuri who may contact you.</span></div>
+        <input type="submit" value="send message" id='btn__submit' />
       </form>
 
       <Footer />
